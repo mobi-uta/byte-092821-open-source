a1f596a2bf420ec8a79a45edcaa0b6b173570868
Long Nguyen <18nguyenl@gmail.com>
2021-09-27 11:16:43 -0500

    Add random offsets

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,6 @@
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/styles");
+    eleventyConfig.addFilter("randomRange", function (value, min, max) {
+        return value + (Math.random() * (max - min) + min);
+    });
 }
